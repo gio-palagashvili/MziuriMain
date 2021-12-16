f87bbd729eda6ba0939abd5a70543bda015e95a2
giopalaga <girgi1201@gmail.com>
2021-12-16 05:56:48 +0400

    Dark mode and login design

--- MainProject/Form1.Designer.cs
+++ MainProject/Form1.Designer.cs
@@ -35,29 +35,33 @@ namespace MainProject
             this.password = new System.Windows.Forms.Label();
             this.Log = new System.Windows.Forms.Button();
             this.register = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(99, 168);
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Location = new System.Drawing.Point(178, 123);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(187, 25);
+            this.textBox1.Size = new System.Drawing.Size(390, 35);
             this.textBox1.TabIndex = 0;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(99, 216);
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Location = new System.Drawing.Point(178, 184);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(187, 25);
+            this.textBox2.Size = new System.Drawing.Size(390, 35);
             this.textBox2.TabIndex = 1;
             // 
             // Username
             // 
             this.Username.AutoSize = true;
             this.Username.ForeColor = System.Drawing.Color.Black;
-            this.Username.Location = new System.Drawing.Point(96, 152);
+            this.Username.Location = new System.Drawing.Point(178, 104);
             this.Username.Name = "Username";
             this.Username.Size = new System.Drawing.Size(55, 13);
             this.Username.TabIndex = 2;
@@ -67,7 +71,7 @@ namespace MainProject
             // 
             this.password.AutoSize = true;
             this.password.ForeColor = System.Drawing.Color.Black;
-            this.password.Location = new System.Drawing.Point(96, 199);
+            this.password.Location = new System.Drawing.Point(178, 166);
             this.password.Name = "password";
             this.password.Size = new System.Drawing.Size(53, 13);
             this.password.TabIndex = 3;
@@ -76,10 +80,12 @@ namespace MainProject
             // Log
             // 
             this.Log.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.Log.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.Log.Location = new System.Drawing.Point(99, 271);
+            this.Log.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Log.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Log.ForeColor = System.Drawing.Color.Transparent;
+            this.Log.Location = new System.Drawing.Point(178, 230);
             this.Log.Name = "Log";
-            this.Log.Size = new System.Drawing.Size(186, 32);
+            this.Log.Size = new System.Drawing.Size(390, 35);
             this.Log.TabIndex = 4;
             this.Log.Text = "Log in";
             this.Log.UseVisualStyleBackColor = false;
@@ -88,20 +94,31 @@ namespace MainProject
             // register
             // 
             this.register.AutoSize = true;
+            this.register.Cursor = System.Windows.Forms.Cursors.Hand;
             this.register.ForeColor = System.Drawing.Color.Blue;
-            this.register.Location = new System.Drawing.Point(99, 252);
+            this.register.Location = new System.Drawing.Point(178, 273);
             this.register.Name = "register";
             this.register.Size = new System.Drawing.Size(46, 13);
             this.register.TabIndex = 5;
             this.register.Text = "Register";
             this.register.Click += new System.EventHandler(this.register_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(325, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Sign in";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(373, 513);
+            this.BackColor = System.Drawing.Color.GhostWhite;
+            this.ClientSize = new System.Drawing.Size(696, 386);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.register);
             this.Controls.Add(this.Log);
             this.Controls.Add(this.password);
@@ -113,10 +130,11 @@ namespace MainProject
             this.Name = "Form1";
             this.RightToLeftLayout = true;
             this.Text = "Form1";
-            this.TransparencyKey = System.Drawing.Color.FromArgb(((int) (((byte) (0)))), ((int) (((byte) (0)))), ((int) (((byte) (0)))), ((int) (((byte) (3)))));
+            this.TransparencyKey = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(3)))));
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
+
         }
 
         #endregion
@@ -127,6 +145,7 @@ namespace MainProject
         private System.Windows.Forms.Label password;
         private System.Windows.Forms.Button Log;
         private System.Windows.Forms.Label register;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- MainProject/Form1.cs
+++ MainProject/Form1.cs
@@ -1,15 +1,38 @@
 ﻿using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace MainProject
 {
     public partial class Form1 : Form
     {
+      
         public Form1()
         {
             InitializeComponent();
+            CssDark();
+        }
+        private void CssDark()
+        {
+            BackColor = ColorTranslator.FromHtml("#1B1C22");
+            Username.ForeColor = Color.White;
+            password.ForeColor = Color.White;
+            textBox1.BackColor = ColorTranslator.FromHtml("#25262C");
+            textBox2.BackColor = ColorTranslator.FromHtml("#25262C");
+            Log.BackColor = ColorTranslator.FromHtml("#3077E3");
+            Log.FlatStyle = FlatStyle.Flat;
+            Log.FlatAppearance.BorderColor = ColorTranslator.FromHtml("#25262C");
+            register.ForeColor = Color.White;
+            Log.Font = new Font("Arial", 10,FontStyle.Bold);
+            label1.Font = new Font("Arial", 12, FontStyle.Bold);
         }
 
+        private void CssLight()
+        {
+            BackColor = ColorTranslator.FromHtml("#FAFBFD");
+            textBox1.BackColor = ColorTranslator.FromHtml("#FAFAFA");
+            textBox2.BackColor = ColorTranslator.FromHtml("#25262C");
+        }
         private void Form1_Load(object sender, EventArgs e)
         {
         }
@@ -33,5 +56,6 @@ namespace MainProject
                 }
             }
         }
+       
     }
 }
