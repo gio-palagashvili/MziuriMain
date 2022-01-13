a813dc3cb4966be35634d89b0de835a5854ab5c1
gio735 <68427506+gio735@users.noreply.github.com>
2022-01-13 22:45:42 +0400

    Add files via upload

---
+++ MainProject/PopUp.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows.Forms;
+
+namespace MainProject
+{
+    public partial class PopUp : Form
+    {
+        public string text { get; set; }
+        public string Route { get; set; }
+        public PopUp()
+        {
+            InitializeComponent();
+            label2.Text = text;
+        }
+
+        private void button2_Click(object sender, System.EventArgs e)
+        {
+            switch (Route)
+            {
+                case "login":
+                    Hide();
+                    var form1 = new Form1();
+                    form1.ShowDialog();
+                    break;
+            }
+        }
+    }
+}
